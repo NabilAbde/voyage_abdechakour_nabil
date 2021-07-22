b329a39fea48a56e151fef8474b65ddf8b11c6d8
[nabil] <[consonab@gmail.com]>
2021-07-22 15:55:23 +0100

    ajout bdd et page admin s affichait avant connexion

---
+++ public/voyage.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 22 juil. 2021 à 14:53
+-- Version du serveur :  5.7.31
+-- Version de PHP : 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `voyage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `description`) VALUES
+(1, 'Mer', 'Vacances à la mer'),
+(2, 'Montagne', 'Vacances à la montagne'),
+(3, 'Campagne', 'Vacances à la campagne'),
+(4, 'Ville', 'Vacances à la ville');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20210722095528', '2021-07-22 09:55:41', 60),
+('DoctrineMigrations\\Version20210722100113', '2021-07-22 10:01:22', 68),
+('DoctrineMigrations\\Version20210722124940', '2021-07-22 12:49:48', 70);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `name`, `firstname`) VALUES
+(1, 'consonab@gmail.com', '[\"ROLE_ADMIN\"]', '123456', 'ABDECHAKOUR', 'Nabil');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voyage`
+--
+
+DROP TABLE IF EXISTS `voyage`;
+CREATE TABLE IF NOT EXISTS `voyage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `accroche` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `priceperperson` int(11) NOT NULL,
+  `duree` int(11) NOT NULL,
+  `image1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image3` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `documentation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `voyage`
+--
+
+INSERT INTO `voyage` (`id`, `name`, `accroche`, `description`, `priceperperson`, `duree`, `image1`, `image2`, `image3`, `documentation`) VALUES
+(1, 'mer Méditerranée', 'Profitez de la cote d \'Azur', 'Les plus belles plages de la cote d\'Azur la M', 400, 7, 'images/coteazur1.jpg', 'images/coteazur2.jpg', 'images/coteazur3.jpg', 'images/coteazur.pdf');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
